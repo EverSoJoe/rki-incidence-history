1f680f1415d5db980909e0020beb1aa749a8c6ea
Josef Eberle <git.josef@jo-e.de>
2021-05-15 23:14:55 +0200

    make bare structure

---
+++ python/update-html.py
@@ -0,0 +1,18 @@
+import openpyxl
+import requests
+import tempfile
+
+def get-rik-xmlx(url, path)
+    pass
+    return file
+
+def get-data(file)
+    return data
+
+def update-html(data)
+    pass
+
+if __name__ == "__main__":
+    file = get-rki-xmlx("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab.xlsx?__blob=publicationFile")
+    data = get-data(file)
+    update-html
