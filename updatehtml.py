84d1c30d7d6f08b02e6da4c5dd9b175cabfd3412
Josef Eberle <git.josef@jo-e.de>
2021-05-25 22:16:36 +0200

    update at Tue 25 May 2021 10:16:36 PM CEST

--- updatehtml.py
+++ updatehtml.py
@@ -25,10 +25,9 @@ def getdata(file):
         negCol = -2
         while sheet[5][negCol].value == None:
             negCol -= 1
-            date = sheet[5][(negCol-14):negCol]
     else:
         negCol = None
-        date = sheet[5][-14:]
+    date = sheet[5][((0 if negCol == None else negCol)-14):negCol]
     data['date'] = [x.value for x in date][::-1]
 
     for row in sheet.iter_rows(min_row=6, values_only=True):
